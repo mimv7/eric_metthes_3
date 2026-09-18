6782144fbf11625dedc810319ea73aa6ae83757e
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-18 18:58:37 +0300

    add 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,6 @@
 # 2.1
+from selenium import webdriver
+
 print('hello, Python, world!')
 
 # 2.2
@@ -11,3 +13,5 @@ print(message)
 url = 'https://stepik.org/lesson/1166614/step/7?unit=1178936'
 no_starch_url = url.removeprefix('https://')
 print(no_starch_url)
+
+driver = webdriver.Chrome()
