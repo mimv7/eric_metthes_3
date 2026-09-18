f19c65f0ad50c572c11c3f6f7d95c9297b2975a9
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-18 18:31:10 +0300

    add 2.py

--- 2.py
+++ 2.py
@@ -6,3 +6,8 @@ message = 'hello'
 print(message)
 message = 'Hi!'
 print(message)
+
+# 2.2/1
+url = 'https://stepik.org/lesson/1166614/step/7?unit=1178936'
+no_starch_url = url.removeprefix('https://')
+print(no_starch_url)
