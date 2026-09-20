37b672de3d8a32ee2ab2c5e716985a5f87a48de0
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-20 12:55:47 +0300

    mod 3.py

--- 3.py
+++ 3.py
@@ -4,4 +4,8 @@ print(names[0].title())
 print(names[1].title())
 print(names[2])
 
-# 3.2
+# 3.3
+guitars =['cort', 'ibanez','yamaha']
+print(f'I would like to by {guitars[0].title()}!')
+print(f'I would like to by {guitars[1].title()}!')
+print(f'I would like to by {guitars[2].title()}!')
