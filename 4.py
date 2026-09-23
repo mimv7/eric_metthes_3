cdcc1cc96f9d9d4f7445687220fc21e8cf7ccc5e
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-23 21:16:04 +0300

    add 4.py

--- 4.py
+++ 4.py
@@ -3,3 +3,26 @@ pizzas = ['4 cheeses', 'margarita', 'dodo']
 for pizza in pizzas:
     print(f'I love pizza {pizza.title()}')
 print('\ti love so match pizza')
+
+# 4.3
+for num in range(1,21):
+    print(num)
+
+# 4.4
+thousand_num = list(range(1,1_001))
+print(thousand_num)
+print(min(thousand_num))
+print(max(thousand_num))
+print(sum(thousand_num))
+
+# 4.6
+odd_mum = list(range(1,21,2))
+print(odd_mum)
+
+# 4.7
+three = list(range(3,31,3))
+print(three)
+
+# 4.8
+cubes = [num ** 3 for num in range(1,11)]
+print(cubes)
